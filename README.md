0891bae56e4ad229a9ec5f8063edfe1bf5833b60
Logan <1308802900@qq.com>
2018-01-19 22:10:55 +0800

    add result.txt, to record some performance values

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # caffe_prototxt
+train and test on cifar10
+50K images for train, 256 for 1 batch
+10K images for test, 100 for 1 batch
--- cifar10/my_time.sh
+++ cifar10/my_time.sh
@@ -7,11 +7,6 @@ TOOLS=$CAFFEHOME/build/tools
 #$TOOLS/caffe train \
 #    --solver=cifar10/solver.prototxt $@
 
-$TOOLS/caffe test \
-    --gpu=all \
-    --model=cifar10/train_val.prototxt   \
-    --weights=alex_adam_iter_10000.caffemodel   \
-    --iterations=100
 $TOOLS/caffe time \
     --gpu=all \
     --model=cifar10/train_val.prototxt   \
